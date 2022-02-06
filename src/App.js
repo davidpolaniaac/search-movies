b8df6706915757c959fd1aa3d0fc19ebf67639ad
David Polania <davidpolaniaac@users.noreply.github.com>
2022-02-06 18:15:44 -0500

    add search movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Home } from './pages/Home'
+import Detail from './pages/Detail'
+import { NotFound } from './pages/NotFound'
+
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/detail/:movieId' element={<Detail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
